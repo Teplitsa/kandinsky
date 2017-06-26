9e66fa1318a3872c776dda5e364ffe6407c2e1e9
Ahaenor <ahaenor@gmail.com>
2017-06-26 14:44:03 +0300

    * Localization fixes.

--- wp-content/themes/kandinsky/core/settings.php
+++ wp-content/themes/kandinsky/core/settings.php
@@ -204,57 +204,15 @@ function knd_register_required_plugins() {
             /* translators: %s: plugin name. */
             'updating'                        => __( 'Updating Plugin: %s', 'knd' ),
             'oops'                            => __( 'Something went wrong with the plugin API.', 'knd' ),
-            'notice_can_install_required'     => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'This theme requires the following plugin: %1$s.',
-                'This theme requires the following plugins: %1$s.',
-                'knd'
-            ),
-            'notice_can_install_recommended'  => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'This theme recommends the following plugin: %1$s.',
-                'This theme recommends the following plugins: %1$s.',
-                'knd'
-            ),
-            'notice_ask_to_update'            => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
-                'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
-                'knd'
-            ),
-            'notice_ask_to_update_maybe'      => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'There is an update available for: %1$s.',
-                'There are updates available for the following plugins: %1$s.',
-                'knd'
-            ),
-            'notice_can_activate_required'    => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'The following required plugin is currently inactive: %1$s.',
-                'The following required plugins are currently inactive: %1$s.',
-                'knd'
-            ),
-            'notice_can_activate_recommended' => _n_noop(
-                /* translators: 1: plugin name(s). */
-                'The following recommended plugin is currently inactive: %1$s.',
-                'The following recommended plugins are currently inactive: %1$s.',
-                'knd'
-            ),
-            'install_link'                    => _n_noop(
-                'Begin installing plugin',
-                'Begin installing plugins',
-                'knd'
-            ),
-            'update_link' 					  => _n_noop(
-                'Begin updating plugin',
-                'Begin updating plugins',
-                'knd'
-            ),
-            'activate_link'                   => _n_noop(
-                'Begin activating plugin',
-                'Begin activating plugins',
-                'knd'
-            ),
+            'notice_can_install_required'     => _n_noop('This theme requires the following plugin: %1$s.', 'This theme requires the following plugins: %1$s.', 'knd'),
+            'notice_can_install_recommended'  => _n_noop('This theme recommends the following plugin: %1$s.', 'This theme recommends the following plugins: %1$s.', 'knd'),
+            'notice_ask_to_update'            => _n_noop('The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.', 'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.', 'knd'),
+            'notice_ask_to_update_maybe'      => _n_noop('There is an update available for: %1$s.', 'There are updates available for the following plugins: %1$s.', 'knd'),
+            'notice_can_activate_required'    => _n_noop('The following required plugin is currently inactive: %1$s.', 'The following required plugins are currently inactive: %1$s.', 'knd'),
+            'notice_can_activate_recommended' => _n_noop('The following recommended plugin is currently inactive: %1$s.', 'The following recommended plugins are currently inactive: %1$s.', 'knd'),
+            'install_link'                    => _n_noop('Begin installing plugin', 'Begin installing plugins', 'knd'),
+            'update_link' 					  => _n_noop('Begin updating plugin', 'Begin updating plugins', 'knd'),
+            'activate_link'                   => _n_noop('Begin activating plugin', 'Begin activating plugins', 'knd'),
             'return'                          => __( 'Return to Required Plugins Installer', 'knd' ),
             'plugin_activated'                => __( 'Plugin activated successfully.', 'knd' ),
             'activated_successfully'          => __( 'The following plugin was activated successfully:', 'knd' ),
