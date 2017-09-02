69a69e1b95b439b4e1a66780fe7062b12b99256d
DenisCherniatev <denis.cherniatev@gmail.com>
2017-09-02 15:56:47 +0200

    * minor fix

--- wp-content/themes/kandinsky/page-home.php
+++ wp-content/themes/kandinsky/page-home.php
@@ -35,11 +35,13 @@ endif;
     
     $menu_items = wp_get_nav_menu_items(__( 'Kandinsky projects block menu', 'knd' ));
     $project_menu_items = [];
-    foreach($menu_items as $k => $v) {
-        $project_menu_items[] = array(
-            'title' => $v->title,
-            'url' => $v->url,
-        );
+    if($menu_items && is_array($menu_items)) {
+        foreach($menu_items as $k => $v) {
+            $project_menu_items[] = array(
+                'title' => $v->title,
+                'url' => $v->url,
+            );
+        }
     }
     
     knd_show_posts_shortlist($projects, "ПРОЕКТЫ «ЛИНИИ ЦВЕТА»", $project_menu_items);
