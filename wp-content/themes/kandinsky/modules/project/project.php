1ff50b16843ca74330113164e746bd8393556dda
DenisCherniatev <denis.cherniatev@gmail.com>
2017-09-04 00:05:04 +0200

    * project widget fixed

--- wp-content/themes/kandinsky/modules/project/project.php
+++ wp-content/themes/kandinsky/modules/project/project.php
@@ -10,7 +10,7 @@ class KND_Project {
     public static function get_short_list($num = 3) {
         
         //query
-        $posts = get_posts(array('post_type' => 'post', 'posts_per_page' => $num));
+        $posts = get_posts(array('post_type' => 'project', 'posts_per_page' => $num));
         
         return $posts;
     }
