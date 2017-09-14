e5c1f62b47a5f2e6d23382f056b639e09fd15128
Anna Ladoshkina <webdev@foralien.com>
2017-09-14 08:33:14 +0300

    Fix for empty category in import

--- wp-content/themes/kandinsky/modules/org/tax.php
+++ wp-content/themes/kandinsky/modules/org/tax.php
@@ -4,7 +4,6 @@ class KND_OrgCategory {
     
     public static function setup_starter_data() {
         wp_insert_term(__('Partners', 'knd'), 'org_cat', array('slug' => 'partners'));
-        wp_insert_term(__('Homepage Partners', 'knd'), 'org_cat', array('slug' => 'home-partners'));
     }
     
 }
