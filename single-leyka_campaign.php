e38d9258fa2fc530bfff552975c58ac96c47871b
Anna Ladoshkina <webdev@foralien.com>
2017-09-21 14:35:29 +0300

    SASS file structure changed

--- single-leyka_campaign.php
+++ single-leyka_campaign.php
@@ -8,12 +8,27 @@ $cpost = get_queried_object();
 $age = get_post_meta($cpost->ID, 'campaign_age', true);
 if(!empty($age))
 	$age = ', '.$age;
- 
+
+
+$is_finished = get_post_meta($cpost->ID, 'is_finished', true);
+
 get_header();
 ?>
 <article class="main-content leyka-campaign">
 <div class="container">
-	<header class="entry-header-full">
+
+	<header class="entry-header-single container-text">
+        <div class="entry-meta">
+        <?php if($is_finished) { ?>
+            <a href="<?php echo site_url('/campaign/completed/'); ?>" class="entry-link">
+                <?php _e('They alredy got help', 'knd') ?>
+            </a> 
+        <?php } else { ?>
+            <a href="<?php echo site_url('/campaign/active/'); ?>" class="entry-link">
+                <?php _e('They need help', 'knd') ?>
+            </a> 
+        <?php } ?>
+        </div>
 		<h1 class="entry-title"><?php echo get_the_title($cpost);?><?php echo $age;?></h1>
 	</header>
 	
@@ -22,16 +37,12 @@ get_header();
 		<div class="campaign-form ">
 			<?php echo apply_filters('the_content', $cpost->post_content); ?>
 		</div>
+
     
-    
-        <section class="addon related-card-holder">
-        
-        <div class="related-cards-loop">
+        <div class="related-campaigns">
             <a href="<?php echo site_url('/campaign/active/'); ?>" class="entry-link"><?php _e('They need help', 'knd') ?></a> 
             <a href="<?php echo site_url('/campaign/completed/'); ?>" class="entry-link"><?php _e('They alredy got help', 'knd') ?></a> 
         </div>
-        
-        </section>
     
 	</main>
 	
