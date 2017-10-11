c221ebe2e9dc50f2ffbf86fc0ea1a8d40da0b88e
Alexey Sidorenko <sidorenko.a@gmail.com>
2017-10-11 12:47:19 +0200

    Revert " * "Require" refactoring - WIP."
    This reverts commit 6a057e5b6811d2beca28c6e314c365ae2c592745.

--- functions.php
+++ functions.php
@@ -63,47 +63,30 @@ require_once( ABSPATH . 'wp-admin/includes/image.php' );
 require_once( ABSPATH . 'wp-admin/includes/media.php' );
 
 // enqueue CSS and JS and compose inline CSS to set vars from settings
-//require get_template_directory().'/core/class-cssjs.php';
-get_template_part('/core/class', 'cssjs');
+require get_template_directory().'/core/class-cssjs.php';
 
-//require get_template_directory().'/core/media.php'; // customize media behavior and add images sizes
-get_template_part('/core/media'); // customize media behavior and add images sizes
-
-//require get_template_directory().'/core/cards.php'; // layout of cards, list items etc.
-get_template_part('/core/cards'); // layout of cards, list items etc.
-//require get_template_directory().'/core/extras.php'; // default WP behavior customization
-get_template_part('/core/extras'); // default WP behavior customization
-
-//require get_template_directory().'/core/shortcodes.php'; // shortcodes core
-get_template_part('/core/shortcodes'); // shortcodes core
-
-//require get_template_directory().'/core/shortcodes-ui.php'; // shortcodes layout
-get_template_part('/core/shortcodes', 'ui'); // shortcodes layout
-
-//get_template_directory().'/core/template-tags.php'; // independent pages parts layout
-get_template_part('/core/template', 'tags'); // independent pages parts layout
-//
-//require get_template_directory().'/core/widgets.php'; // setup widgets
-get_template_part('/core/widgets'); // setup widgets
-get_template_part('/core/', 'customizer'); // WP theme customizer setup
+require get_template_directory().'/core/media.php'; // customize media behavior and add images sizes
+require get_template_directory().'/core/cards.php'; // layout of cards, list items etc.
+require get_template_directory().'/core/extras.php'; // default WP behavior customization
+require get_template_directory().'/core/shortcodes.php'; // shortcodes core
+require get_template_directory().'/core/shortcodes-ui.php'; // shortcodes layout 
+require get_template_directory().'/core/template-tags.php'; // independent pages parts layout 
+require get_template_directory().'/core/widgets.php'; // setup widgets
+require get_template_directory().'/core/customizer.php'; // WP theme customizer setup
 
 // import data utils
-//require get_template_directory().'/core/class-mediamnt.php'; // tools for work with files
-get_template_part('/core/class', 'mediamnt'); // tools for work with files
+require get_template_directory().'/core/class-mediamnt.php'; // tools for work with files
+require get_template_directory().'/core/class-import.php'; // import files into site media lib
+require get_template_directory().'/core/import.php'; // import helpers
 
-//require get_template_directory().'/core/class-import.php'; // import files into site media lib
-get_template_part('/core/class', 'import'); // import files into site media lib
-
-//require get_template_directory().'/core/import.php'; // import helpers
-get_template_part('/core/import'); // import helpers
-//require get_template_directory().'/core/import.php'; // import helpers
-
-// Include modules
+// include modules
 foreach (glob(get_template_directory() . "/modules/*") as $module_file) {
+    
     if(is_dir($module_file)) {
-        $php_filename = basename($module_file); //. '.php';
-        $php_file = $module_file . '/' . $php_filename;
-    } else {
+        $php_filename = basename($module_file) . '.php';
+        $php_file = $module_file . "/" . $php_filename;
+    }
+    else {
         $php_file = $module_file;
     }
     
@@ -113,14 +96,12 @@ foreach (glob(get_template_directory() . "/modules/*") as $module_file) {
 }
 
 if(is_admin() || current_user_can('manage_options')) {
-//    require get_template_directory() . '/core/admin.php';
-    get_template_part('/core/admin');
-    get_template_part('/vendor/class/tgm-plugin-activation');
+    require get_template_directory() . '/core/admin.php';
+    require get_template_directory().'/vendor/class-tgm-plugin-activation.php';
 }
 
 if((is_admin() && !empty($_GET['page']) && $_GET['page'] == 'knd-setup-wizard' ) || wp_doing_ajax()) {
-//    require get_template_directory().'/vendor/envato_setup/envato_setup.php'; // Run the wizard after all modules included
-    get_template_part('/vendor/envato_setup/','envato_setup'); // Run the wizard after all modules included
+    require get_template_directory().'/vendor/envato_setup/envato_setup.php'; // Run the wizard after all modules included
 }
 
 // Service lines (to localize):
