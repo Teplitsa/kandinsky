0b378f558a15a87ed58a6b1d19ad282883af260a
DenisCherniatev <denis.cherniatev@gmail.com>
2017-10-17 23:16:37 +0200

    * update flag removed

--- footer.php
+++ footer.php
@@ -17,7 +17,6 @@ $footer_text = get_theme_mod('footer_text');
 <div id="bottom_bar" class="bottom-bar">
     <div class="container">
         <div class="flex-row align-bottom bottom-branding">
-            <h1>Update in FOOTER</h1>
             <div class="flex-cell flex-md-5">
                 <h1 class="logo-name"><?php bloginfo('name');?></h1>
                 <h2 class="logo-name"><?php bloginfo('description');?></h2>
