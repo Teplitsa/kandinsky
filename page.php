c4e3afda21c471884f24892a9a29cd27359eb7d7
Ahaenor <ahaenor@gmail.com>
2018-02-26 16:44:19 +0300

    * Fix for non-existent the_content filter in the page template.

--- page.php
+++ page.php
@@ -7,25 +7,25 @@
 $cpost = get_queried_object();
 
 
-get_header(); 
+get_header();
 ?>
-<header class="page-header">
-    <div class="container"><div class="text-column">
-    <?php if($cpost->post_parent > 0) {?>
-        <div class="page-crumb"><a href="<?php echo get_permalink($cpost->post_parent);?>"><?php echo get_the_title($cpost->post_parent);?></a></div>
-    <?php }?>
-        <h1 class="page-title"><?php echo get_the_title($cpost);?></h1>
-        <?php if(!empty($cpost->post_excerpt)) { ?>
-            <div class="page-intro"><?php echo apply_filters('knd_the_title', $cpost->post_excerpt);?></div>
-        <?php } ?>
-    </div></div>
-</header>
+    <header class="page-header">
+        <div class="container"><div class="text-column">
+                <?php if($cpost->post_parent > 0) {?>
+                    <div class="page-crumb"><a href="<?php echo get_permalink($cpost->post_parent);?>"><?php echo get_the_title($cpost->post_parent);?></a></div>
+                <?php }?>
+                <h1 class="page-title"><?php echo get_the_title($cpost);?></h1>
+                <?php if($cpost->post_excerpt) {?>
+                    <div class="page-intro"><?php echo apply_filters('knd_the_title', $cpost->post_excerpt);?></div>
+                <?php } ?>
+            </div></div>
+    </header>
 
-<div class="page-content container">
-    <div class="the-content text-column">
-        <?php echo apply_filters('knd_entry_the_content', $cpost->post_content); ?>
+    <div class="page-content container">
+        <div class="the-content text-column">
+            <?php echo apply_filters('the_content', apply_filters('knd_entry_the_content', $cpost->post_content));?>
+        </div>
     </div>
-</div>
 
 
-<?php get_footer(); 
+<?php get_footer();
