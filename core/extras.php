9d67c3c2a92b554a623c41e531c2e01f0a8beed6
Ahaenor <ahaenor@gmail.com>
2019-02-22 17:04:51 +0300

    * create_function() call removed as deprecated.

--- core/extras.php
+++ core/extras.php
@@ -38,7 +38,7 @@ add_filter( 'knd_entry_the_content', 'do_shortcode', 11 );
 add_filter( 'knd_entry_the_content', 'wp_kses_post', 7 );
 
 /* jpeg compression */
-add_filter( 'jpeg_quality', create_function( '', 'return 95;' ) );
+add_filter('jpeg_quality', function(){ return 95; });
 
 /* temp fix for wpautop in posts */
 function knd_entry_wpautop( $content ) {
