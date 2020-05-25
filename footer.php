4662c5712db4f8bf440eadef6e70357d31e6a350
BootWP <burdianov@gmail.com>
2020-05-25 19:03:25 +0300

    Coding Standards: Improve header.php and footer.php code standart

--- footer.php
+++ footer.php
@@ -4,68 +4,71 @@
  *
  * Contains the closing of the #content div and all content after
  *
- * @package bb
+ * @package Kandinsky
  */
 
-$cc_link = '<a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">Creative Commons СС-BY-SA 3.0</a>';
-
+$cc_link = '<a href="http://creativecommons.org/licenses/by-sa/3.0/" target="_blank">' . esc_html__( 'Creative Commons СС-BY-SA 3.0', 'knd' ) . '</a>';
 
 $footer_text = knd_get_theme_mod('footer_text');
+
 ?>
 </div><!--  #site_content -->
 
 <div id="bottom_bar" class="bottom-bar">
-    <div class="container">
-        <div class="flex-row align-bottom bottom-branding">
-            <div class="flex-cell flex-md-5">
-                <h1 class="logo-name"><?php bloginfo('name');?></h1>
-                <h2 class="logo-name"><?php bloginfo('description');?></h2>
-            </div>
+	<div class="container">
+		<div class="flex-row align-bottom bottom-branding">
+			<div class="flex-cell flex-md-5">
+				<h1 class="logo-name"><?php bloginfo( 'name' );?></h1>
+				<h2 class="logo-name"><?php bloginfo( 'description' );?></h2>
+			</div>
 
-            <div class="flex-cell flex-md-7 links-right">
-            <?php 
-                $social_icons = knd_social_links(array(), false);
-                if($social_icons) {
-                    echo $social_icons;
-                }
-            ?>
-            </div>
+			<div class="flex-cell flex-md-7 links-right">
+				<?php
+				$social_icons = knd_social_links( array(), false );
+				if ( $social_icons ) {
+					echo $social_icons;
+				}
+				?>
+			</div>
 
-        </div>
-    </div>
+		</div>
+	</div>
 </div>
 
-<footer class="site-footer"><div class="container">		
-	
-	<div class="widget-area"><?php dynamic_sidebar( 'knd-footer-sidebar' );?></div>
-    
-	<div class="hr"></div>
-    
-	<div class="flex-row footer-credits align-center">
-		
-		<div class="flex-cell flex-mf-8  flex-md-6">		
-				
-			<div class="copy">
-				<?php echo apply_filters('knd_the_content', $footer_text); ?>	
-				<p><?php printf(__('All materials of the site are avaliabe under license %s', 'knd'), $cc_link);?></p>
+<footer class="site-footer">
+	<div class="container">
+
+		<div class="widget-area"><?php dynamic_sidebar( 'knd-footer-sidebar' );?></div>
+
+		<div class="hr"></div>
+
+		<div class="flex-row footer-credits align-center">
+
+			<div class="flex-cell flex-mf-8 flex-md-6">
+
+				<div class="copy">
+					<?php echo apply_filters( 'knd_the_content', $footer_text ); ?>
+					<p><?php printf( __( 'All materials of the site are avaliabe under license %s', 'knd' ), $cc_link ); ?></p>
+				</div>
+
 			</div>
-			
-		</div>
-		
-		<div class="flex-cell flex-mf-4 flex-md-6">
-			<div class="knd-brand">
-                <a title="<?php esc_attr_e("Project Kandinsky", 'knd');?>" href="<?php echo KND_OFFICIAL_WEBSITE_URL;?>" target="_blank">
-				<div class="support"><?php echo esc_html__( 'Powered by Kandinsky', 'knd' ); ?></div>
-				<div class="knd-banner"><svg class="knd-icon pic-knd"><use xlink:href="#pic-knd" /></svg></div>
-				</a>
-			</div>			
+
+			<div class="flex-cell flex-mf-4 flex-md-6">
+				<div class="knd-brand">
+					<a title="<?php esc_attr_e( 'Project Kandinsky', 'knd');?>" href="<?php echo esc_attr( KND_OFFICIAL_WEBSITE_URL ); ?>" target="_blank">
+						<div class="support"><?php esc_html_e( 'Powered by Kandinsky', 'knd' ); ?></div>
+						<div class="knd-banner"><svg class="knd-icon pic-knd"><use xlink:href="#pic-knd" /></svg></div>
+					</a>
+				</div>
+			</div>
+
 		</div>
 
 	</div>
-	
-	
-</div></footer>
+
+</footer>
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <?php
 /**
  * The header for our theme.
+ *
+ * @package Kandinsky
  */
+
 ?><!DOCTYPE html>
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
@@ -14,55 +17,62 @@
 </head>
 
 <body id="top" <?php body_class(); ?>>
-<?php include_once(get_template_directory()."/assets/svg/svg.svg"); //all svgs ?>
+<?php include_once get_template_directory() . '/assets/svg/svg.svg'; // all svgs. ?>
 <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'knd' ); ?></a>
 
 <header id="site_header" class="site-header">
-	
+
 	<div class="site-header-panel">
-			
+
 		<div class="site-panel-row">
-        
-			<div class="site-branding site-panel-cell"><?php knd_logo_markup();?></div>
-			
+
+			<div class="site-branding site-panel-cell"><?php knd_logo_markup(); ?></div>
+
 			<?php $header_text = knd_get_theme_mod('text_in_header'); ?>
 			<div class="site-details site-panel-cell">
-				<div class="site-details-cell"><?php echo apply_filters('knd_the_content', $header_text); ?></div>
-			</div>									
-			
+				<div class="site-details-cell"><?php echo apply_filters( 'knd_the_content', $header_text ); ?></div>
+			</div>
+
 			<div class="trigger-button donate site-panel-cell hide-upto-medium">
-				<a id="trigger_donate"  href="<?php echo knd_get_theme_mod('knd_hero_image_support_url') ?>"><?php echo knd_get_theme_mod('knd_hero_image_support_button_caption'); ?></a>
+				<a id="trigger_donate"  href="<?php echo knd_get_theme_mod( 'knd_hero_image_support_url' ) ?>"><?php echo knd_get_theme_mod('knd_hero_image_support_button_caption'); ?></a>
 			</div>
-			
+
 			<div class="trigger-button menu site-panel-cell">
-				<a id="trigger_menu" href="<?php echo home_url('sitemap');?>">
-					<?php knd_svg_icon('icon-menu');?>					
-				</a>				
+				<a id="trigger_menu" href="<?php echo esc_url( home_url( 'sitemap' ) ); ?>">
+					<?php knd_svg_icon( 'icon-menu' ); ?>
+				</a>
 			</div>
-			
+
 		</div>
-        	
+
 	</div>
-	
 
 	<div class="nav-overlay"></div>
 	<nav id="site_nav" class="site-nav">
 		<div class="site-nav-title">
-			
+
 			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="snt-cell">
 				<h1 class="logo-name"><?php bloginfo('name'); ?></h1>
 				<h2 class="logo-name hide-upto-medium"><?php bloginfo('description'); ?></h2>
 			</a>
-			
-			<div id="trigger_menu_close" class="trigger-button close"><?php knd_svg_icon('icon-close');?></div>
+
+			<div id="trigger_menu_close" class="trigger-button close"><?php knd_svg_icon( 'icon-close' ); ?></div>
 		</div>
 		<?php
-			$after = '<span class="submenu-trigger">'.knd_svg_icon('icon-up', false).knd_svg_icon('icon-down', false).'</span>';
-			wp_nav_menu(array('menu' => __('Main menu', 'knd'), 'theme_location' => 'primary', 'container' => false, 'menu_class' => 'main-menu', 'after' => $after));
+		$after = '<span class="submenu-trigger">' . knd_svg_icon( 'icon-up', false ) . knd_svg_icon( 'icon-down', false ) . '</span>';
+		wp_nav_menu(
+			array(
+				'menu'           => esc_html__('Main menu', 'knd'),
+				'theme_location' => 'primary',
+				'container'      => false,
+				'menu_class'     => 'main-menu',
+				'after'          => $after,
+			)
+		);
 		?>
 		<div class="search-holder"><?php get_search_form();?></div>
-		<?php knd_social_links();?>
-	</nav>		
+		<?php knd_social_links(); ?>
+	</nav>
 </header>
 
 <div id="site_content" class="site-content"><a name="#content"></a>
