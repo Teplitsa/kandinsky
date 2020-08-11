9572cc39db85d859ba3e288be5bd108ad3d6ae96
BootWP <burdianov@gmail.com>
2020-08-11 07:53:30 +0300

    Improve WPCS

--- functions.php
+++ functions.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * bb functions and definitions
+ * Kandinsky functions and definitions
  *
- * @package bb
+ * @package Kandinsky
  */
 
-if ( ! defined( 'WPINC' ) )
+if ( ! defined( 'WPINC' ) ) {
 	die();
+}
 
 define('KND_VERSION', '1.4.0');
 define('KND_DOC_URL', 'https://github.com/Teplitsa/kandinsky/wiki/');
@@ -20,35 +21,46 @@ define('KND_SUPPORT_URL', 'https://pd.te-st.ru/forms/contacts/');
 define('KND_SUPPORT_TELEGRAM', 'https://t.me/joinchat/AAAAAENN3prSrvAs7KwWrg');
 define('KND_SETUP_WIZARD_URL', admin_url('themes.php?page=knd-setup-wizard'));
 define('KND_DISTR_ARCHIVE_URL', 'https://github.com/Teplitsa/kandinsky/archive/master.zip');
-#define('KND_DISTR_ARCHIVE_URL', 'https://github.com/Teplitsa/kandinsky/archive/dev.zip');
+/** define('KND_DISTR_ARCHIVE_URL', 'https://github.com/Teplitsa/kandinsky/archive/dev.zip'); */
 define('KND_MIN_PHP_VERSION', '5.6.0');
 define('KND_PHP_VERSION_ERROR_MESSAGE', '<strong>Внимание:</strong> версия PHP ниже <strong>5.6.0</strong>. Кандинский нуждается в PHP хотя бы <strong>версии 5.6.0</strong>, чтобы работать корректно.<br /><br />Пожалуйста, направьте вашему хостинг-провайдеру запрос на повышение версии PHP для этого сайта.');
 
-if( !defined('PHP_VERSION') || version_compare(PHP_VERSION, KND_MIN_PHP_VERSION, '<') ) {
-
-  function sample_admin_notice__success() {
-	?>
-	<div class="notice notice-error">
-		<p><?php echo KND_PHP_VERSION_ERROR_MESSAGE;?></p>
-	</div>
-	<?php
-  }
-  add_action( 'admin_notices', 'sample_admin_notice__success' );
+if ( ! defined('PHP_VERSION') || version_compare(PHP_VERSION, KND_MIN_PHP_VERSION, '<') ) {
+	/**
+	 * Admin notice
+	 */
+	function knd_admin_notice__success() {
+		?>
+		<div class="notice notice-error">
+			<p><?php echo wp_kses_post( KND_PHP_VERSION_ERROR_MESSAGE ); ?></p>
+		</div>
+		<?php
+	}
+	add_action( 'admin_notices', 'knd_admin_notice__success' );
 }
 
-if( !isset($content_width) ) {
+if ( ! isset( $content_width ) ) {
 	$content_width = 800; /* pixels */
 }
 
+/**
+ * Load text domain
+ */
 function knd_load_theme_textdomain() {
 	load_theme_textdomain( 'knd', get_template_directory() . '/lang' );
 }
 
+/**
+ * Theme Setup
+ */
 function knd_setup() {
 
-	// Inits
+	// Inits.
 	knd_load_theme_textdomain();
-	//add_theme_support( 'automatic-feed-links' );	
+
+	// Support automatic feed links.
+	add_theme_support( 'automatic-feed-links' );
+
 	add_theme_support('title-tag');
 
 	/**
@@ -68,32 +80,32 @@ function knd_setup() {
 		)
 	);
 
-	// Menus
+	// Menus.
 	$menus = array(
-		'primary'   => __('Primary menu', 'knd'),
-		//'social'    => 'Социальные кнопки',
-		//'sitemap'   => 'Карта сайта'
+		'primary'   => esc_html__( 'Primary menu', 'knd' ),
 	);
 
-	register_nav_menus($menus);
+	register_nav_menus( $menus );
 
-	// Editor style
-	add_editor_style(array('assets/css/editor.css'));
+	// Editor style.
+	add_editor_style( array( 'assets/css/editor.css' ) );
 
-	// Support automatic feed links
-	add_theme_support( 'automatic-feed-links' );
+	// Add support for full and wide align images.
+	add_theme_support( 'align-wide' );
 
 }
-add_action( 'after_setup_theme', 'knd_setup', 9 ); // Theme wizard initialize at 10, this init should occur befure it
+add_action( 'after_setup_theme', 'knd_setup', 9 ); // Theme wizard initialize at 10, this init should occur befure it.
 
 /* Fix translate customizer */
 if ( is_customize_preview() ) {
 	knd_load_theme_textdomain();
 }
 
-/* Function for init setting that should be runned at init hook */
+/**
+ * Function for init setting that should be runned at init hook
+ */
 function knd_content_init() {
-	add_post_type_support('page', 'excerpt');
+	add_post_type_support( 'page', 'excerpt' );
 }
 add_action( 'init', 'knd_content_init', 30 );
 
@@ -101,12 +113,12 @@ add_action( 'init', 'knd_content_init', 30 );
  * Includes
  */
 
-// WP libs to operate with files and media
-require_once( ABSPATH . 'wp-admin/includes/file.php' );
-require_once( ABSPATH . 'wp-admin/includes/image.php' );
-require_once( ABSPATH . 'wp-admin/includes/media.php' );
+// WP libs to operate with files and media.
+require_once ABSPATH . 'wp-admin/includes/file.php';
+require_once ABSPATH . 'wp-admin/includes/image.php';
+require_once ABSPATH . 'wp-admin/includes/media.php';
 
-get_template_part('/core/extras'); // default WP behavior customization
+get_template_part( '/core/extras' ); // default WP behavior customization.
 
 /**
  * Template functions
@@ -123,52 +135,52 @@ require_once get_theme_file_path( '/core/customizer/customizer.php' );
  */
 require_once get_theme_file_path( '/core/typography.php' );
 
-// enqueue CSS and JS and compose inline CSS to set vars from settings
+// enqueue CSS and JS and compose inline CSS to set vars from settings.
 
-get_template_part('/core/class-cssjs');
+get_template_part( '/core/class-cssjs' );
 
-get_template_part('/core/media'); // customize media behavior and add images sizes
+get_template_part( '/core/media' ); // customize media behavior and add images sizes.
 
-get_template_part('/core/cards'); // layout of cards, list items etc.
+get_template_part(  '/core/cards' ); // layout of cards, list items etc.
 
-get_template_part('/core/shortcodes'); // shortcodes core
-get_template_part('/core/shortcodes-ui'); // shortcodes layout
+get_template_part( '/core/shortcodes' ); // shortcodes core.
+get_template_part( '/core/shortcodes-ui' ); // shortcodes layout.
 
-get_template_part('/core/template-tags'); // independent pages parts layout
+get_template_part( '/core/template-tags' ); // independent pages parts layout.
 
-get_template_part('/core/widgets'); // setup widgets
+get_template_part( '/core/widgets' ); // setup widgets.
 
-// import data utils
-get_template_part('/core/class-mediamnt'); // tools for work with files
+// import data utils.
+get_template_part( '/core/class-mediamnt' ); // tools for work with files.
 
-get_template_part('/core/class-import'); // import files into site media lib
-get_template_part('/core/import'); // import files into site media lib
+get_template_part( '/core/class-import' ); // import files into site media lib.
+get_template_part( '/core/import' ); // import files into site media lib.
 
-// Include modules
-foreach (glob(get_template_directory() . '/modules/*') as $module_file) {
-	if(is_dir($module_file)) {
-		$php_filename = basename($module_file) . '.php';
-		$php_file = $module_file . '/' . $php_filename;
+// Include modules.
+foreach ( glob( get_template_directory() . '/modules/*' ) as $module_file ) {
+	if ( is_dir( $module_file ) ) {
+		$php_filename = basename( $module_file ) . '.php';
+		$php_file     = $module_file . '/' . $php_filename;
 	} else {
 		$php_file = $module_file;
 	}
 
-	if(is_file($php_file) && preg_match('/.*\.php$/', $php_file)) {
-		require( $php_file );
+	if ( is_file( $php_file ) && preg_match( '/.*\.php$/', $php_file ) ) {
+		require $php_file;
 	}
 }
 
-if(is_admin() || current_user_can('manage_options')) {
-	get_template_part('/core/admin-update-theme');
-	get_template_part('/core/admin');
-	get_template_part('/vendor/class-tgm-plugin-activation');
+if ( is_admin() || current_user_can( 'manage_options' ) ) {
+	get_template_part( '/core/admin-update-theme' );
+	get_template_part( '/core/admin' );
+	get_template_part( '/vendor/class-tgm-plugin-activation' );
 }
 
-if((is_admin() && !empty($_GET['page']) && $_GET['page'] == 'knd-setup-wizard' ) || wp_doing_ajax()) {
-	get_template_part('/vendor/envato_setup/envato_setup'); // Run the wizard after all modules included
+if ( ( is_admin() && ! empty( $_GET['page'] ) && $_GET['page'] == 'knd-setup-wizard' ) || wp_doing_ajax() ) {
+	get_template_part( '/vendor/envato_setup/envato_setup' ); // Run the wizard after all modules included.
 }
 
-// Service lines (to localize):
-__('Kandinsky', 'knd');
-__('Teplitsa', 'knd');
-__('The beautiful design and useful features for nonprofit website', 'knd');
+// Service lines (to localize):.
+esc_html__('Kandinsky', 'knd');
+esc_html__('Teplitsa', 'knd');
+esc_html__('The beautiful design and useful features for nonprofit website', 'knd');
