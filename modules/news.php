9eb9ef16698fddd5879e423b31a296aac65c40aa
BootWP <burdianov@gmail.com>
2021-04-02 12:29:05 +0300

    Add View More link in news block.

--- modules/news.php
+++ modules/news.php
@@ -14,9 +14,10 @@ class KND_News_Widget extends WP_Widget {
 			return;
 		}
 
-		$title = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
-		$num = intval( $instance['num'] );
-
+		$title     = apply_filters( 'widget_title', $instance['title'], $instance, $this->id_base );
+		$num       = intval( $instance['num'] );
+		$more_text = isset( $instance['more_text'] ) ? $instance['more_text'] : '';
+		$more_url  = isset( $instance['more_url'] ) ? $instance['more_url'] : '';
 		// num.
 		if ( $num <= 0 ) {
 			$num = 3;
@@ -28,10 +29,10 @@ class KND_News_Widget extends WP_Widget {
 			'posts_per_page' => $num,
 		) );
 
-		self::print_widget( $posts, $args, $title );
+		self::print_widget( $posts, $args, $title, $more_text, $more_url );
 	}
 
-	public static function print_widget( $posts, $args, $title ) {
+	public static function print_widget( $posts, $args, $title, $more_text, $more_url ) {
 		extract( $args );
 
 		echo $before_widget;
@@ -41,7 +42,18 @@ class KND_News_Widget extends WP_Widget {
 
 	<div class="container">
 
-		<h2 class="section-title"><?php echo wp_kses_post( $title ); ?></h2>
+		<div class="section-heading">
+			<h2 class="section-title"><?php echo wp_kses_post( $title ); ?></h2>
+			<?php if ( $more_text && $more_url ) { ?>
+				<div class="section-links">
+					<a href="<?php echo esc_url( $more_url ); ?>"><?php echo esc_html( $more_text ); ?>
+						<svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M12.0303 6.53033C12.3232 6.23744 12.3232 5.76256 12.0303 5.46967L7.25736 0.696699C6.96447 0.403806 6.48959 0.403806 6.1967 0.696699C5.90381 0.989593 5.90381 1.46447 6.1967 1.75736L10.4393 6L6.1967 10.2426C5.90381 10.5355 5.90381 11.0104 6.1967 11.3033C6.48959 11.5962 6.96447 11.5962 7.25736 11.3033L12.0303 6.53033ZM0 6.75H11.5V5.25H0V6.75Z" fill="currentColor"/>
+						</svg>
+					</a>
+				</div>
+			<?php } ?>
+		</div>
 
 		<div class="flex-row start cards-row">
 		<?php
@@ -80,14 +92,33 @@ class KND_News_Widget extends WP_Widget {
 		name="<?php echo $this->get_field_name( 'num' ); ?>" type="text"
 		value="<?php echo intval( $instance['num'] ); ?>">
 </p>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'more_text' );?>"><?php esc_html_e( 'More Text:','knd' ); ?></label>
+	<input class="widefat"
+		id="<?php echo $this->get_field_id( 'more_text' ); ?>"
+		name="<?php echo $this->get_field_name( 'more_text' ); ?>" type="text"
+		value="<?php echo esc_attr( $instance['more_text'] ); ?>">
+</p>
+
+<p>
+	<label for="<?php echo $this->get_field_id( 'more_url' );?>"><?php esc_html_e( 'More Url:','knd' ); ?></label>
+	<input class="widefat"
+		id="<?php echo $this->get_field_id( 'more_url' ); ?>"
+		name="<?php echo $this->get_field_name( 'more_url' ); ?>" type="text"
+		value="<?php echo esc_attr( $instance['more_url'] ); ?>">
+</p>
+
 <?php
 	}
 
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 
-		$instance['title'] = sanitize_text_field( $new_instance['title'] );
-		$instance['num'] = intval( $new_instance['num'] );
+		$instance['title']     = sanitize_text_field( $new_instance['title'] );
+		$instance['num']       = intval( $new_instance['num'] );
+		$instance['more_text'] = sanitize_text_field( $new_instance['more_text'] );
+		$instance['more_url']  = sanitize_text_field( $new_instance['more_url'] );
 
 		return $instance;
 	}
