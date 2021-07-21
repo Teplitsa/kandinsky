c06910f6568c071fd8a7af1b06a9d1a5815e7dd1
BootWP <burdianov@gmail.com>
2021-07-21 11:02:07 +0300

    Improve header.php

--- header.php
+++ header.php
@@ -1,18 +1,15 @@
 <?php
 /**
- * The header for our theme.
+ * The header.
  *
  * @package Kandinsky
  */
 
-?><!DOCTYPE html>
-<html class="no-js" <?php language_attributes(); ?>>
+?><!doctype html>
+<html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head(); ?>
 </head>
 
