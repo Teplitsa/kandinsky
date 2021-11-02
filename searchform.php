7a16793282a797c9d0c21012fba4660a3e8fd261
BootWP <burdianov@gmail.com>
2021-11-02 10:37:07 +0200

    Add label for search imput.

--- searchform.php
+++ searchform.php
@@ -2,9 +2,12 @@
 /**
  * The template for displaying search forms 
  */
+$knd_unique_id = wp_unique_id( 'search-form-' );
+
 ?>
 	<form method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+		<label for="<?php echo esc_attr( $knd_unique_id ); ?>" class="screen-reader-text"><?php esc_html_e( 'Search', 'knd' ); ?></label>
 		<?php knd_svg_icon('icon-search');?>
-		<input type="search" class="search-field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s"  placeholder="<?php esc_attr_e('Find', 'knd');?>" autocomplete="off">
-        <span class="action"><?php esc_html_e('Press Enter', 'knd');?></span>		
+
+		<input type="search" class="search-field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="<?php echo esc_attr( $knd_unique_id ); ?>"  placeholder="<?php esc_attr_e('Find', 'knd');?>" autocomplete="off">
 	</form>
