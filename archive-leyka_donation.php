45fde643fc3453a3ca28899ce064433f080dabc0
BootWP <burdianov@gmail.com>
2021-11-25 10:49:39 +0200

    Update donation archive template.

--- archive-leyka_donation.php
+++ archive-leyka_donation.php
@@ -8,7 +8,7 @@ $campaign = null;
 
 if($campaign_slug) {	
 	if($campaign = get_posts(array('post_type' => 'leyka_campaign', 'post_name' => $campaign_slug)))
-		$campaign = reset($campaign);		
+		$campaign = reset($campaign);
 }
 
 get_header();
@@ -23,33 +23,36 @@ get_header();
 </div>
 
 <div class="container-text main-content">
-<section class="donations-history-results donation_history">
-<?php
-	if(have_posts() && class_exists('Leyka_Donation')){
-		foreach($wp_query->posts as $p){
-			$donation = new Leyka_Donation($p);
-			$amount = number_format($donation->sum, 0, '.', ' ');
-			
-			echo "<div class='ldl-item'>";
-			echo "<div class='amount'>{$amount} {$donation->currency_label}</div>";
-			
-			$meta = array();
-			$name = $donation->donor_name;
-			$name = (!empty($name)) ? $name : __('Anonymous', 'knd');				
-			$meta[] = '<span>'.$name.'</span>';
-			
-			$meta[] = '<time>'.$donation->date_funded.'</time>';
-			echo "<div class='meta'>".implode(' / ', $meta)."</div>";
-			echo "</div>";
+
+	<section class="donations-history-results donation_history">
+	<?php
+		if(have_posts() && class_exists('Leyka_Donation')){
+			foreach($wp_query->posts as $p){
+				$donation = new Leyka_Donation($p);
+				$amount = number_format($donation->sum, 0, '.', ' ');
+
+				echo "<div class='ldl-item'>";
+				echo "<div class='amount'>{$amount} {$donation->currency_label}</div>";
+
+				$meta = array();
+				$name = $donation->donor_name;
+				$name = (!empty($name)) ? $name : __('Anonymous', 'knd');
+				$meta[] = '<span>'.$name.'</span>';
+
+				$meta[] = '<time>'.$donation->date_funded.'</time>';
+				echo "<div class='meta'>".implode(' / ', $meta)."</div>";
+				echo "</div>";
+			}
 		}
-	}
-	else {
-		echo "<p>";
-		echo "</p>";
-	}
-?>
-</section>
-<section class="paging"><?php knd_paging_nav($wp_query); ?></section>
+		else {
+			echo "<p>";
+			echo "</p>";
+		}
+	?>
+	</section>
+
+	<?php knd_posts_pagination( array( 'screen_reader_text' => esc_html__( 'Donations navigation', 'knd' ) ) ); ?>
+
 </div>
 
 <?php get_footer();
