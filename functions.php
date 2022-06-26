030241c82eb84f055a92bb1f3a19b8e695d6133d
BootWP <burdianov@gmail.com>
2022-06-26 09:57:13 +0300

    Update version 2.2.3

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ if ( ! defined( 'WPINC' ) ) {
 // 	@ ini_set( 'display_errors', 1 );
 // }
 
-define('KND_VERSION', '2.2.2');
+define('KND_VERSION', '2.2.3');
 define('KND_DOC_URL', 'https://knd.te-st.ru/docs/');
 define('KND_OFFICIAL_WEBSITE_URL', 'https://knd.te-st.ru/');
 define('KND_OFFICIAL_SUPPORT_URL', 'https://knd.te-st.ru/bug/');
