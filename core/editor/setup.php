cbbc4f38c0d8333a6c812833ebf40b6a18a51168
BootWP <burdianov@gmail.com>
2024-05-08 13:35:10 +0300

    Improve support WP version 6.5

--- core/editor/setup.php
+++ core/editor/setup.php
@@ -16,17 +16,8 @@ function knd_gutenberg_setup() {
 	// Add support for full and wide align images.
 	add_theme_support( 'align-wide' );
 
-	//add_theme_support( 'wp-block-styles' );
-
 	add_theme_support( 'custom-spacing' );
 
-	//add_theme_support( 'custom-units', 'rem', 'em', 'px' );
-
-
-	// Disable Custom Colors
-	//add_theme_support( 'disable-custom-colors' );
-
-	//add_theme_support('experimental-link-color');
 	add_theme_support('link-color');
 
 	add_theme_support( 'custom-line-height' );
@@ -36,14 +27,3 @@ function knd_gutenberg_setup() {
 
 }
 add_action( 'after_setup_theme', 'knd_gutenberg_setup' );
-
-
-function knd_admin_menu_blocks() {
-	$url_edit = 'edit.php?post_type=wp_block';
-
-	if ( $url_edit ) {
-		add_menu_page( esc_html__( 'Blocks', 'knd' ), esc_html__( 'Blocks', 'knd' ), 'edit_pages', $url_edit, null, 'dashicons-screenoptions', 20 );
-	}
-
-}
-add_action( 'admin_menu', 'knd_admin_menu_blocks' );
